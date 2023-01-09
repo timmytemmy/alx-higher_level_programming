670d62dec779ee07229d66711a4a74a621e2202f
Timilehin <113606191+timmytemmy@users.noreply.github.com>
2023-01-09 19:14:38 +0100

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-number = 0
-while number <= 89:
-	if number % 10 == 0:
-		number += 1 + number // 10
-	print("{:02d}".format(number), end='\n' if number == 89 else ", ")
-	number += 1
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ", end="")
+print("")
