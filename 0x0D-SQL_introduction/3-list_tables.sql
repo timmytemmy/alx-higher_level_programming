0161a478116d4685687fc8899ab15e560f11b7bd
Timilehin <113606191+timmytemmy@users.noreply.github.com>
2023-03-16 12:42:06 +0100

    Task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
-Write a script that lists all the tables of a database in your MySQL server.
-
-mysql> SHOW TABLES;
+-- Write a script that lists all the tables of a database in your MySQL server.
+SHOW TABLES;
